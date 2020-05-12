d3cf2b14ba9b0eb815170e72ffab49abaf075f5d
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2020-05-12 23:27:49 +0200

    Add useWhyDidYouUpdate hook (#39)
    * Add useWhyDidYouUpdate hook
    
    * Remove typescript type

---
+++ lib/hooks/useWhyDidYouUpdate.ts
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+// Performance hook to see which props cause a rerender of the component.
+// TODO: Check if props type could be more defined.
+export function useWhyDidYouUpdate(name: string, props: any): void {
+  const previousProps = React.useRef<HTMLElement>();
+
+  React.useEffect(() => {
+    if (previousProps.current) {
+      // get all keys from previous and current props.
+      const allKeys = Object.keys({ ...previousProps.current, ...props });
+      // Use this object to keep track of changed props.
+      const changes = {};
+
+      allKeys.forEach((key) => {
+        // If previous is different from current
+        if (previousProps.current[key] !== props[key]) {
+          // add to changesObj
+          changes[key] = {
+            from: previousProps.current[key],
+            to: props[key]
+          };
+        }
+      });
+
+      // If changes is not empty then output to console.
+      if (Object.keys(changes).length) {
+        console.log('[why-did-you-update]', name, changes);
+      }
+    }
+    // Finally update previousProps with current props for next hook call.
+    previousProps.current = props;
+  });
+}
