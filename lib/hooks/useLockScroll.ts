e4f05e451aa73477eaa06e01c8265ada8ecf35a2
Emiel Van Severen <emielvanseveren@outlook.com>
2020-05-12 23:42:05 +0200

    Add two lockscroll hooks

---
+++ lib/hooks/useLockScroll.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export function useLockBodyScroll(): void {
+  React.useLayoutEffect(() => {
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = 'hidden';
+    return (): void => { document.body.style.overflow = originalStyle; };
+  });
+}
+
+export function useLockRefScroll(ref: React.MutableRefObject<HTMLElement>): void {
+  console.log('this is the ref', ref.current);
+
+  React.useLayoutEffect(() => {
+    const originalStyle = ref.current.style.overflow;
+    ref.current.style.overflow = 'hidden';
+    return (): any => { ref.current.style.overflow = originalStyle; };
+  });
+}
