2cd0179d0a250fb770af82c1b16631400e217871
Emiel Van Severen <emielvanseveren@outlook.com>
2020-05-14 00:08:49 +0200

    Remove dead imports

--- lib/hooks/useModal/index.tsx
+++ lib/hooks/useModal/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { createPortal } from 'react-dom';
-import { useLockBodyScroll, useLockRefScroll } from 'lib/hooks';
 import { Container, Overlay } from './style';
 
 interface IModalProps {
