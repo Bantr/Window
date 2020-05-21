4eabe1f8d450cbbe5738df13763e3023544dc331
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2020-05-21 23:36:01 +0200

    Add useOnScreen hook (#51)

---
+++ lib/hooks/useOnScreen.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+export function useOnScreen(ref: React.RefObject<any>, rootMargin = '0px'): boolean {
+  const [isIntersecting, setIntersecting] = React.useState(false);
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin
+      }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return (): void => {
+      observer.unobserve(ref.current);
+    };
+  }, []);
+
+  return isIntersecting;
+}
