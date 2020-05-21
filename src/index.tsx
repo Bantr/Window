5d9c0e62fed89727598d985cbf1c4780d2654c7a
Emiel Van Severen <emielvanseveren@outlook.com>
2020-05-21 23:13:59 +0200

    Add whydidyourender to detect unnecessary renders

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as Sentry from '@sentry/browser';
 import { isDevelopment } from 'lib/util';
 import { App } from './app/App';
+
+// should become lazy loaded
+import * as Sentry from '@sentry/browser';
+import * as whyDidYouRender from '@welldone-software/why-did-you-render';
 
 // Sentry should only throw errors in production.
 // TODO: helper function for process env
 if (!isDevelopment()) {
   Sentry.init({ dsn: process.env.SENTRY_DSN });
+}
+
+if (isDevelopment()) {
+  whyDidYouRender.default(React, { trackAllPureComponents: true });
 }
 
 const root = document.getElementById('root');
