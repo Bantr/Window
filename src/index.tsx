c974e4ee651ad14189a528431573ec5b23a2090e
Emiel Van Severen <emielvanseveren@outlook.com>
2020-07-20 20:25:04 +0200

    Fix mistake sentry react being wrapper around sentry/browser and importing both

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { isDevelopment } from 'lib/util';
 import { bantrSettings } from 'lib/settings';
-import { App } from './app/App';
+import App from './app/App';
 
-import * as Sentry from '@sentry/browser';
+import * as Sentry from '@sentry/react';
 import { Integrations } from '@sentry/apm';
 
 import * as whyDidYouRender from '@welldone-software/why-did-you-render';
+
+console.log(Sentry);
 
 // Sentry should only throw errors in production.
 if (!isDevelopment()) {
@@ -15,10 +17,9 @@ if (!isDevelopment()) {
     dsn: process.env.SENTRY_DSN,
     release: `${bantrSettings.clientAppName}${bantrSettings.clientAppVersion}`,
     integrations: [
-      // @ts-ignore (No advanced types for this new feature yet.)
       new Integrations.Tracing({ tracingOrigins: ['api.bantr.app'] })
     ],
-    tracesSampleRate: 0.2 // catches 20% of api calls
+    tracesSampleRate: 0.2 // catches 20% of calls
   });
 
   // Helps to find unnecessary renders
