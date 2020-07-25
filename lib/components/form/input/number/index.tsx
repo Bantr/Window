ea73d0d4ffd0071d79a25d174585580a9d018e12
Emiel Van Severen <emielvanseveren@outlook.com>
2020-07-26 00:02:23 +0200

    Add simple number input

--- lib/components/form/input/number/index.tsx
+++ lib/components/form/input/number/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
-import { Container, LabelContainer, Label, InputContainer, Input, ErrorContainer, Error } from '../input/style';
+import { Container, Error, ErrorContainer, Input, InputContainer, Label, LabelContainer } from '../input/style';
+import { ChangeNumberContainer } from './style';
 import { IInputDefaultProps } from '../input/defaultProps';
+import { Arrow } from 'lib/icons';
 
-export const TextField = React.forwardRef<HTMLInputElement, IInputDefaultProps>(({ labelText, placeholder, name, error, icon, readOnly, loading = false }, ref) => {
+export const NumberField = React.forwardRef<HTMLInputElement, IInputDefaultProps>(({ labelText, placeholder, name, error, icon, loading, readOnly }, ref) => {
   const [showError, setShowError] = React.useState(false);
+  const [value, setValue] = React.useState<any>();
 
   if (loading) {
     return (
@@ -29,11 +32,17 @@ export const TextField = React.forwardRef<HTMLInputElement, IInputDefaultProps>(
           id={name}
           name={name}
           onBlur={(): void => { setShowError(false); }}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>): void => { if (!error) { setValue(e.currentTarget.value); } }}
           onFocus={(): void => { setShowError(true); }}
           placeholder={placeholder}
           readOnly={readOnly}
           ref={ref}
+          value={value ? value : ''}
         />
+        <ChangeNumberContainer>
+          <Arrow className="add" onClick={(): void => { if (!error) { setValue(parseInt(value) + 1); } }} pointer rotate={-90} />
+          <Arrow className="substract" onClick={(): void => { if (!error) { setValue(parseInt(value) - 1); } }} pointer rotate={90} />
+        </ChangeNumberContainer>
       </InputContainer>
       {
         error ?
