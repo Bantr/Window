0be6b384eaa6c7b7955d7547c92dae3384365b13
Emiel Van Severen <emielvanseveren@outlook.com>
2020-09-02 17:34:36 +0200

    Add graphql schema to graphql file

--- scripts/getGraphqlSchema.sh
+++ scripts/getGraphqlSchema.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .env
-gq https://graphql.bantr.app -H "bantr-graphql: $HASURA_GRAPHQL_USER_SECRET"  --introspect > ./lib/graphql/schema.graphql
+gq https://graphql.bantr.app -H "bantr-graphql: $HASURA_GRAPHQL_USER_SECRET"  --introspect > ./lib/graphql/schema.gql
